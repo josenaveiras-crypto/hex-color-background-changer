244c53ee804c20dc28b8cfb90f71f70df4a80f6c
josenaveiras-crypto <jose.naveiras@gmail.com>
2026-09-02 13:04:07 +0200

    solucion

--- js/app.js
+++ js/app.js
@@ -3,3 +3,20 @@ const colorBox = document.querySelector('#color-box');
 const hexValue = document.querySelector('#hex-value');
 
 // ¡Buena suerte!
+button.addEventListener("click", generarColor);
+
+function generarColor() {
+    console.log("CLICK CORRECTO");
+    console.log(generarColorHexadecimal());
+    const colorAleatorio = generarColorHexadecimal();
+    hexValue.textContent = colorAleatorio;
+    colorBox.style.backgroundColor = colorAleatorio;
+
+}
+
+function generarColorHexadecimal() {
+  return "#" + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0");
+}
+
